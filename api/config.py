0273301eed3ffdf76f5ae2d18b50107a3d59adf7
Yagnesh upadhyay <uyagnesh88@gmail.com>
2026-07-07 21:48:59 +0530

    fix database

--- api/config.py
+++ api/config.py
@@ -2,12 +2,12 @@ from pydantic_settings import BaseSettings
 from pydantic import ConfigDict
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password : str
-    database_name: str
-    database_username: str
-
+    # database_hostname: str
+    # database_port: str
+    # database_password : str
+    # database_name: str
+    # database_username: str
+    database_url: str
 
     secretkey: str
     algorithm: str
